c23a1e3edefed58aa62b6456d1a4c9cb82abaf42
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-05-24 02:41:55 +0900

    feat: add async await example

--- BACK-END/controllers/admin.js
+++ BACK-END/controllers/admin.js
@@ -10,51 +10,45 @@ exports.createSurvey = async (req, res, next) => {
   const { hasExpiry, closeAt, isPublic } = req.body;
   const pages = req.body.pages;
 
-  pages.forEach((page, pagesIdx) => {
-    const pageTitle = page.title;
-    const pageDescription = page.description;
-    let elements = page.elements;
+  const pageObjs = [];
 
-    elements = elements.map((element) => {
-      return new Question({ ...element });
-    });
+  try {
+    for (const page of pages) {
+      const elements = page.elements.map(
+        (element) => new Question({ ...element })
+      );
 
-    const pageObjs = [];
+      const insertedQuetions = await Question.insertMany(elements);
+      const insertedPage = { ...page, elements: insertedQuetions };
 
-    Question.insertMany(elements)
-      .then((result) => {
-        pageObjs.push({
-          title: pageTitle,
-          description: pageDescription,
-          elements: result,
-        });
-        if (pagesIdx === pages.length - 1) {
-          if (closeAt) {
-            return Survey.create({
-              creatorKey: creatorKey,
-              hasExpiry: hasExpiry,
-              isPublic: isPublic,
-              closeAt: new Date(closeAt),
-              pages: pageObjs,
-            });
-          }
-          return Survey.create({
-            creatorKey: creatorKey,
-            hasExpiry: hasExpiry,
-            isPublic: isPublic,
-            pages: pageObjs,
-          });            
-        }
-      })
-      .then((survey) => {
-        return insertCreatorInfo(survey, creatorKey);
-      })
-      .then(() => {
-        return res.status(201).json({ MESSAGE: "SUCCESS" });
-      })
-      .catch((error) => {
-        console.log(error);
-        return res.status(400).json({ ERROR: error });
+      pageObjs.push(insertedPage);
+    }
+
+    let survey;
+
+    if (closeAt) {
+      survey = await Survey.create({
+        creatorKey: creatorKey,
+        hasExpiry: hasExpiry,
+        isPublic: isPublic,
+        closeAt: new Date(closeAt),
+        pages: pageObjs,
       });
-  });
+    }
+
+    if (!closeAt) {
+      survey = await Survey.create({
+        creatorKey: creatorKey,
+        hasExpiry: hasExpiry,
+        isPublic: isPublic,
+        pages: pageObjs,
+      });
+    }
+
+    await insertCreatorInfo(survey, creatorKey);
+    res.status(201).json({ MESSAGE: "SUCCESS" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ ERROR: error });
+  }
 };
